f884623956ae2e15a260a6f669e0e92d4eafb065
bluesgao <bluesgao@126.com>
2025-09-16 00:56:41 +0800

    first commit

---
+++ project_test_data.sql
@@ -0,0 +1,249 @@
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        1,
+        '2025-07-17T16:03:44.767326',
+        '2025-07-18T16:03:44.767326',
+        'Project 1',
+        'This is a description for project 1.',
+        'https://picsum.photos/200/300?random=1',
+        'Film',
+        6315,
+        3334,
+        14,
+        1814,
+        '2025-09-11T16:03:44.767348',
+        '2025-10-11T16:03:44.767348',
+        'successful',
+        '0x11f308a0cb6e45',
+        'Creator 1',
+        '0x9e5d40e33f69e',
+        '0xdc97ef11530bb0a1baafa31bd3'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        2,
+        '2025-08-05T16:03:44.767382',
+        '2025-08-07T16:03:44.767382',
+        'Project 2',
+        'This is a description for project 2.',
+        'https://picsum.photos/200/300?random=2',
+        'Tech',
+        5024,
+        2623,
+        29,
+        551,
+        '2025-09-11T16:03:44.767388',
+        '2025-10-02T16:03:44.767388',
+        'pending',
+        '0x1a24b1aad92f0a',
+        'Creator 2',
+        '0xd4d23f7892f85',
+        '0xdea71f788e2abc576563d949d3'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        3,
+        '2025-08-01T16:03:44.767402',
+        '2025-08-07T16:03:44.767402',
+        'Project 3',
+        'This is a description for project 3.',
+        'https://picsum.photos/200/300?random=3',
+        'Art',
+        4594,
+        2411,
+        77,
+        1445,
+        '2025-09-11T16:03:44.767406',
+        '2025-09-26T16:03:44.767406',
+        'pending',
+        '0x7cb4c19ce82eb',
+        'Creator 3',
+        '0x1bc5691dca8d3a',
+        '0x22bf28c37dae58e39977a9fbe93'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        4,
+        '2025-07-05T16:03:44.767421',
+        '2025-07-06T16:03:44.767421',
+        'Project 4',
+        'This is a description for project 4.',
+        'https://picsum.photos/200/300?random=4',
+        'Tech',
+        7494,
+        1595,
+        40,
+        1026,
+        '2025-09-14T16:03:44.767425',
+        '2025-10-11T16:03:44.767425',
+        'pending',
+        '0x15c440900dd93e',
+        'Creator 4',
+        '0x189828307b7b76',
+        '0x36fd67989fb040ab0af2eb7b7ea'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        5,
+        '2025-07-18T16:03:44.767441',
+        '2025-07-28T16:03:44.767441',
+        'Project 5',
+        'This is a description for project 5.',
+        'https://picsum.photos/200/300?random=5',
+        'Tech',
+        9586,
+        440,
+        94,
+        948,
+        '2025-09-10T16:03:44.767454',
+        '2025-09-26T16:03:44.767454',
+        'failed',
+        '0x1f2d6ebaf052a1',
+        'Creator 5',
+        '0x15dddf15967458',
+        '0x21842df4dbbe52130bc940ca759'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        6,
+        '2025-06-29T16:03:44.767469',
+        '2025-07-05T16:03:44.767469',
+        'Project 6',
+        'This is a description for project 6.',
+        'https://picsum.photos/200/300?random=6',
+        'Music',
+        7966,
+        143,
+        86,
+        731,
+        '2025-09-12T16:03:44.767473',
+        '2025-10-07T16:03:44.767473',
+        'successful',
+        '0x1a53e1d7f0fbd7',
+        'Creator 6',
+        '0x2192fa9fe21a3b',
+        '0x36fbd58d833a3fe8fc05502a8da'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        7,
+        '2025-09-04T16:03:44.767487',
+        '2025-09-13T16:03:44.767487',
+        'Project 7',
+        'This is a description for project 7.',
+        'https://picsum.photos/200/300?random=7',
+        'Education',
+        9556,
+        3407,
+        60,
+        1435,
+        '2025-09-14T16:03:44.767491',
+        '2025-10-03T16:03:44.767491',
+        'pending',
+        '0x14a0e4b8d51693',
+        'Creator 7',
+        '0x16be0d4a5f7ba1',
+        '0xcb30f08d9a31d6d377963fab59'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        8,
+        '2025-06-10T16:03:44.767507',
+        '2025-06-19T16:03:44.767507',
+        'Project 8',
+        'This is a description for project 8.',
+        'https://picsum.photos/200/300?random=8',
+        'Film',
+        5287,
+        902,
+        83,
+        1157,
+        '2025-09-14T16:03:44.767513',
+        '2025-10-13T16:03:44.767513',
+        'pending',
+        '0x1327f3f547f5d6',
+        'Creator 8',
+        '0xdea1afe688365',
+        '0x4d165c6c0ee8a23c0f0d3a7e5fe'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        9,
+        '2025-06-25T16:03:44.767524',
+        '2025-07-01T16:03:44.767524',
+        'Project 9',
+        'This is a description for project 9.',
+        'https://picsum.photos/200/300?random=9',
+        'Music',
+        4662,
+        4280,
+        13,
+        666,
+        '2025-09-11T16:03:44.767526',
+        '2025-10-05T16:03:44.767526',
+        'active',
+        '0x10b275821d9f3b',
+        'Creator 9',
+        '0xeecce0d3a7d80',
+        '0x135767651c847b356a65bb53be1'
+    );
+
+INSERT INTO public.project 
+    (id, created_at, updated_at, title, description, image_url, category, 
+     target_amount, current_amount, min_amount, max_amount, start_time, end_time, 
+     status, creator_address, creator_name, contract_address, transaction_hash)
+    VALUES (
+        10,
+        '2025-07-10T16:03:44.767536',
+        '2025-07-11T16:03:44.767536',
+        'Project 10',
+        'This is a description for project 10.',
+        'https://picsum.photos/200/300?random=10',
+        'Art',
+        9318,
+        4718,
+        65,
+        720,
+        '2025-09-12T16:03:44.767538',
+        '2025-09-29T16:03:44.767538',
+        'active',
+        '0x194e553ff754d4',
+        'Creator 10',
+        '0x68b79570242e2',
+        '0x25ede7045e36b4c962ee8c2bfbc'
+    );
